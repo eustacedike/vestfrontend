560bf8929baae3cb3e1271d70c9da4d560282c01
eustacedike <eustacedyke@gmail.com>
2022-12-28 17:24:19 +0100

    Updates

---
+++ precache-manifest.7d4093a71bbaf4ee99eb9e503db531f7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "a971282cfbabd88982e9",
+    "url": "/vestfrontend/static/css/main.fd98e3ae.chunk.css"
+  },
+  {
+    "revision": "a971282cfbabd88982e9",
+    "url": "/vestfrontend/static/js/main.a971282c.chunk.js"
+  },
+  {
+    "revision": "8db9736a72d6a9f5a5e8",
+    "url": "/vestfrontend/static/js/1.8db9736a.chunk.js"
+  },
+  {
+    "revision": "d6f3692fb2f700712adb",
+    "url": "/vestfrontend/static/js/runtime~main.d6f3692f.js"
+  },
+  {
+    "revision": "21e9800ff701a6ee39bc2dd0be3f4395",
+    "url": "/vestfrontend/static/media/alam.21e9800f.jpg"
+  },
+  {
+    "revision": "a368ed9ebf0904076ad4833685ba004c",
+    "url": "/vestfrontend/static/media/homevid.a368ed9e.mp4"
+  },
+  {
+    "revision": "7d7fab50b53588e6f61c87d75520fe43",
+    "url": "/vestfrontend/static/media/kingery.7d7fab50.jpg"
+  },
+  {
+    "revision": "91335244683c8300b55d2f996435c1cc",
+    "url": "/vestfrontend/static/media/gold.91335244.jpeg"
+  },
+  {
+    "revision": "02942e80b16f2026fb1da00eaeeec333",
+    "url": "/vestfrontend/static/media/realestate.02942e80.jpeg"
+  },
+  {
+    "revision": "57bcb5a232dd4c31db456b3b98a3605e",
+    "url": "/vestfrontend/static/media/loan.57bcb5a2.jpeg"
+  },
+  {
+    "revision": "03fec82f5232903cec05fe41c70320db",
+    "url": "/vestfrontend/static/media/logo1.03fec82f.jpg"
+  },
+  {
+    "revision": "d74791cb14dec9e7d6c7e80e61ee95ca",
+    "url": "/vestfrontend/static/media/backg.d74791cb.jpg"
+  },
+  {
+    "revision": "97f970caea72d93e94004ae2330cbd64",
+    "url": "/vestfrontend/index.html"
+  }
+];
